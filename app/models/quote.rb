1a3da55dbc60c5629979ad7bbbae2d8f21562e13
Ingrid <olmccb@hotmail.com>
2015-11-01 21:24:57 +0000

    Added About page, image and bio

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ActiveRecord::Base
+	validates :saying, :presence => true, :length => { :maximum => 140, :minimun => 3 }
+	validates :author, :presence => true, :length => { :maximum => 50, :minimun => 3 }
 end
